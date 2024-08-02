e6c4fb52f063b1586ff7666990702f14e49d2587
alamgir_hosen <thisisalamgir@gmail.com>
2024-08-02 10:02:52 +0600

    Navbar p-2

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let menu_bar = document.getElementById("menu_bar");
-let menu_items = document.getElementById("menu_items");
+let menu = document.getElementById("menu");
+let ul = document.getElementById("ul");
 
-menu_bar.addEventListener("click", () => {
-  menu_items.classList.toggle("hidden");
-  menu_bar.classList.toggle("hide");
+menu.addEventListener("click", () => {
+  ul.classList.toggle("show");
 });
